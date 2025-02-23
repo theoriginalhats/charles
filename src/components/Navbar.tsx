348884fb6e3122b433de3860c8ed52b5a61fd545
theorigianlhats <95531511+theoriginalhats@users.noreply.github.com>
2025-02-22 22:22:20 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaDiscord } from 'react-icons/fa';
 import { Menu, X, Home, Code, Download, FileText } from 'lucide-react';
